f742ca57da0043770a851ef1d81254813dc925a3
g-2012 <Gduponchel@RKS1306W315.ensg1.ensg.eu>
2014-03-05 11:28:33 +0100

    creation packages gauthier

---
+++ slope3D_/src/gauthierTest/GrilleTest.java
@@ -0,0 +1,26 @@
+package gauthierTest;
+
+public class GrilleTest {  // MNT fictif créé pour les tests
+	
+	public 		double 		pas  = 30	; 		// pas du MNT (m)
+	public 		int 		nLig = 10	; 		// nombre de lignes   de la grille
+	public		int			nCol = 10	; 		// nombre de colonnes de la grille
+	public 		double 		x0   = 0	; 		// abcsisse  du coin supérieur gauche
+	public 		double 		y0   = 0	; 		// ordonnée  du coin supérieur gauche
+	
+	public double[][] valeurs = { 
+			
+			{  70   ,  70   ,  70   ,  70   ,  70   ,  70   ,  70   ,  70   ,  70   ,   70  }, // altitudes des points du MNT (m)
+			{  70   ,  90   ,  90   ,  90   ,  90   ,  90   ,  90   ,  90   ,  90   ,   90  },
+			{  70   ,  90   ,  94   ,  94   ,  94   ,  94   ,  94   ,  94   ,  94   ,   91  },
+			{  70   ,  90   ,  94   ,  95   ,  95   ,  95   ,  95   ,  95   ,  95   ,   94  },
+			{  70   ,  90   ,  94   ,  95   ,  96   ,  96   ,  96   ,  95   ,  95   ,   95  },
+			{  70   ,  92   ,  94   ,  95   ,  96   ,  500  ,  96   ,  95   ,  95   ,   95  },
+			{  70   ,  90   ,  94   ,  95   ,  96   ,  96   ,  96   ,  95   ,  95   ,   95  },
+			{  70   ,  90   ,  94   ,  95   ,  95   ,  95   ,  95   ,  95   ,  95   ,   94  },
+			{  70   ,  90   ,  94   ,  94   ,  94   ,  94   ,  94   ,  94   ,  94   ,   91  },
+			{  70   ,  90   ,  90   ,  90   ,  90   ,  90   ,  90   ,  90   ,  90   ,   90  }
+	
+	};
+	
+} //fin de la classe
---
+++ slope3D_/src/gauthierUtils/GrilleATriangles.java
@@ -0,0 +1,70 @@
+package gauthierUtils;
+
+import gauthierTest.GrilleTest;
+
+import java.util.ArrayList;
+
+
+public abstract class GrilleATriangles {
+	public static void grilleVersTriangles(GrilleTest grille) { // lit la grille fournie pour en extraire les sommets des triangles
+		int nTL = 2*(grille.nCol-1); // nombre de triangles par ligne
+		int nTT = nTL*(grille.nLig-1); // nombre total de triangles dans la grille
+		System.out.println(nTL+" triangles par ligne, "+nTT+" triangles au total.");
+		ArrayList<Triangle> listeT = new ArrayList<>();
+		int i,j;
+		
+		for (int n = 0; n < nTT; n++){
+			i = n / nTL; // indice de la ligne du coin supérieur gauche de la case dans la grille
+			j = (n - i*nTL) / 2; // indice de la colonne du coin supérieur gauche de la case dans la grille
+			
+			if (n%2 == 0){ // indice pair -> triangle droit
+				double a[] = { //sommet supérieur gauche du triangle
+					grille.x0 + j * grille.pas, // absisse du sommet en m (Easting)
+					grille.y0 - i * grille.pas, // ordonnée du sommet en m (Northing)
+					grille.valeurs[i][j] // altitude du sommet
+				};
+				
+				double b[] = { //sommet supérieur droit du triangle
+						grille.x0 + (j+1) * grille.pas,
+						grille.y0 - i * grille.pas,
+						grille.valeurs[i][j+1]
+				};
+				
+				double c[] = { //sommet inférieur gauche du triangle
+						grille.x0 + j * grille.pas,
+						grille.y0 - (i+1) * grille.pas,
+						grille.valeurs[i+1][j]
+				};
+				
+				listeT.add(new Triangle(a, b, c, n));
+			}
+			
+			else if(n%2 == 1) { // indice impair -> triangle inversé
+				double d[] = { //sommet inférieur droit du triangle
+						grille.x0 + (j+1) * grille.pas, // absisse du sommet en m (Easting)
+						grille.y0 - (i+1) * grille.pas, // ordonnée du sommet en m (Northing)
+						grille.valeurs[i+1][j+1] // altitude du sommet
+				};
+				
+				double b[] = { //sommet supérieur droit du triangle
+							grille.x0 + (j+1) * grille.pas,
+							grille.y0 - i * grille.pas,
+							grille.valeurs[i][j+1]
+				};
+				
+				double c[] = { //sommet inférieur gauche du triangle
+							grille.x0 + j * grille.pas,
+							grille.y0 - (i+1) * grille.pas,
+							grille.valeurs[i+1][j]
+				};
+					
+				listeT.add(new Triangle(b, c, d, n));
+			}
+			
+		}
+		
+		/*for (int n = 0; n < nTT; n++){  // affichage pour tester
+			Triangle.afficherCoordonneesPointsTriangle(listeT.get(n));
+		}*/
+	}
+}
