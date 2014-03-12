d5de4b2bfdaf4d05f6a70e556ade1a1d69a43e07
XD <xavier.dumetz@ensg.eu>
2014-03-12 12:32:56 +0100

    MAJ GUI/BarreMenu 12/03/2014-12:32

--- Slope3D_Xav/src/GUI/BarreMenu.java
+++ Slope3D_Xav/src/GUI/BarreMenu.java
@@ -7,13 +7,19 @@ import javax.swing.JMenuBar;
 public class BarreMenu extends JMenuBar {
 	/* Cette classe correspond à la barre de menu qui sera affichée dans la fenêtre du programme. */
 	private MenuFichier mFichier;
+	private MenuReglage mReglages;
 	
 	public BarreMenu(Dimension d) {
 		super();
 		this.setPreferredSize(d);
 		
+		/* Menu relatif à l'ouverture de fichiers et à la fermeture du programme */
 		mFichier = new MenuFichier();
 		this.add(mFichier);
+		
+		/* Menu donnant accès au paramétrage de la visualisation */
+		mReglages = new MenuReglage();
+		this.add(mReglages);
 		
 	}
 
