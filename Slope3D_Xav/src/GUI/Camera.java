735fb21660b20bd827c7e02c5569e8c777b83a80
XD <Xavier.Dumetz@gmail.com>
2014-03-12 16:35:51 +0100

    MAJ GUI/Camera 12/03/2014-16:34 Correction de la fonction setPerspective pour initialiser les variables. Correction des fonctions setCameraXXX pour appeler setPerspective. La classe Camera devient obsolète.

--- Slope3D_Xav/src/GUI/Camera.java
+++ Slope3D_Xav/src/GUI/Camera.java
@@ -14,19 +14,22 @@ public abstract class Camera {
 	L'axe x est horizontal (largeur), l'axe y vertical (hauteur). Ces deux axes sont dans le plan de l'écran.
 	L'axe z est orthogonal à l'écran (profondeur), son origine (z=0) est la surface de l'écran, et les z sont comptés
 	positivement quand on s'éloigne de l'oeil de l'utilisateur. */
-	private double champVertical; // Champ de vision (angle en degrés) dans la direction verticale (de l'écran).
-	private double ratio; // Ratio entre le champ de vison horizontal et le champ vertical = ratio largeur / hauteur de la zone d'affichage.
-    private double profProche; // Profondeur (en unités de distance de l'environnement 3D) du plus proche plan visible. (Doit être strictement positif)
-    private double profLoin; // Profondeur du plan visible le plus éloigné. (Doit être supérieur à profProche) 
+	private static double champVertical; // Champ de vision (angle en degrés) dans la direction verticale (de l'écran).
+	private static double ratio; // Ratio entre le champ de vison horizontal et le champ vertical = ratio largeur / hauteur de la zone d'affichage.
+    private static double profProche; // Profondeur (en unités de distance de l'environnement 3D) du plus proche plan visible. (Doit être strictement positif)
+    private static double profLoin; // Profondeur du plan visible le plus éloigné. (Doit être supérieur à profProche) 
     
     // Réglage de la matrice de projection, qui permet de calculer la mise en perspective de l'image 3D.
-    public void setPerspective(GL2 gl, GLU glu, GLJPanel pan) {
+    public static void setPerspective(GL2 gl, GLU glu, GLJPanel pan) {
         // Définit la matrice de projection GL_PROJECTION comme matrice active.
         gl.glMatrixMode(GL_PROJECTION);
         gl.glLoadIdentity();
 
         // Perspective.
         ratio = (double) (pan.getWidth() / pan.getHeight());
+        champVertical = 45;
+        profProche = 0.1;
+        profLoin = 100;
         glu.gluPerspective(champVertical, ratio, profProche, profLoin);
         
         // Définit la matrice active comme étant à nouveau la matrice de modélisation et d'affichage GL_MODELVIEW.
@@ -34,16 +37,25 @@ public abstract class Camera {
         
     }
     
-    /* Réglage de la position et de l'orientation de la caméra. Ici la caméra est mise en orbite autour de la verticale
-    terrain passant par le centre du MNT et vise le MNT en son centre en contre-plongée. */
-    public void setCameraOrbite(GL2 gl, GLU glu, GLJPanel pan, double angleRot /* Angle de rotation en radians de la caméra en orbite*/, double zPlanOrbital, double rOrbite, double[] coordCentreVue ) {
-    	
+    /* Réglage de la position et de l'orientation de la caméra.
+     * Ici la caméra est mise en orbite autour de la verticale terrain passant par le centre du MNT et vise le MNT en son centre en contre-plongée.
+     */
+    public static void setCameraOrbite(GL2 gl, GLU glu, GLJPanel pan, double angleRot /* Angle de rotation en radians de la caméra en orbite*/, double zPlanOrbital, double rOrbite, double[] coordCentreVue ) {
+    	setPerspective(gl, glu, pan);
         gl.glLoadIdentity();
         // Paramétrage de la vue : caméra en orbite autour de l'axe vertical coupant le MNT en son centre, et visant ce centre
         glu.gluLookAt(rOrbite*Math.cos(angleRot), rOrbite*Math.sin(angleRot), zPlanOrbital, coordCentreVue[0], coordCentreVue[1], coordCentreVue[2], 0, 0, 1);
     }
     
     
+    /* Réglage de la position et de l'orientation de la caméra.
+     * Ici la caméra est placée à la verticale du MNT et l'observe dans une direction parallèle à l'axe central du MNT.
+     */
+    public static void setCameraOrtho(GL2 gl, GLU glu, GLJPanel pan, double[] cam, double[] cible) {
+    	setPerspective(gl, glu, pan);
+    	gl.glLoadIdentity();
+    	glu.gluLookAt(cam[0], cam[1], cam[2], cible[0], cible[1], cible[2], 0, 1, 0);
+    }
     
     
 
