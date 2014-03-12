6a244bf065722c5f532b0dd94313b0e425318a2a
XD <xavier.dumetz@ensg.eu>
2014-03-12 10:08:48 +0100

    Commit initial de la classe GUI/PanneauControle

---
+++ Slope3D_Xav/src/GUI/PanneauControle.java
@@ -0,0 +1,13 @@
+package GUI;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class PanneauControle extends JPanel {
+	public PanneauControle() {
+		super();
+		this.setBackground(Color.GRAY);
+		
+	}
+}
