49321a6d191fc3f3f37fe5b9a8de5a38388e62f3
XD <Xavier.Dumetz@gmail.com>
2014-03-12 16:30:46 +0100

    Création d'une classe de constantes utilisables dans l'intégralité du programme.

---
+++ Slope3D_Xav/src/Utils/Constantes.java
@@ -0,0 +1,28 @@
+package Utils;
+
+/**
+ * Classe contenant des valeurs de constantes utilisées dans le programme
+ */
+public class Constantes {
+	
+	/*
+	 * Constantes liées au choix des objets affichés dans l'environnement 3D
+	 */
+	public static byte OBJ_RIEN = 0;
+	public static byte OBJ_MNT = 1;
+	public static byte OBJ_ISOLIGNES = 2;
+	public static byte OBJ_TOUT = 3;
+
+	/*
+	 * Constantes liées au choix du type de caméra
+	 */
+	public static byte CAM_ORBITE = 0;
+	public static byte CAM_DESSUS = 1;
+	public static byte CAM_PERSO = 2;
+	
+	/*
+	 * Constantes liées au choix du code couleurs pour la pente
+	 */
+	public static boolean COU_AUTO = false;
+	public static boolean COU_PERSO = true;
+}
