5320e1e42f429c65f989db67dd69037253002ca8
XD <Xavier.Dumetz@gmail.com>
2014-03-12 16:32:05 +0100

    MAJ Test/Lanceur 12/03/2014-16:31 Implantation de l'écouteur chargé d'arrêter l'animateur openGL en cas de fermeture de la fenêtre.

--- Slope3D_Xav/src/Test/Lanceur.java
+++ Slope3D_Xav/src/Test/Lanceur.java
@@ -1,11 +1,29 @@
 package Test;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import GUI.*;
 
 public class Lanceur {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Fenetre fen = new Fenetre();
+		final Fenetre fen = new Fenetre();
+		fen.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				// Use a dedicate thread to run the stop() to ensure that the
+				// animator stops before program exits.
+				new Thread() {
+					@Override
+					public void run() {
+						if (((Panneau3D) fen.getPanEnv()).getAnimateur().isStarted()) ((Panneau3D) fen.getPanEnv()).getAnimateur().stop();
+						System.exit(0);
+					}
+				}.start();
+			}
+		});
+
 	}
 
 }
