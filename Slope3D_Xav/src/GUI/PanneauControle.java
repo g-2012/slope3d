bbbc7c275685ad70775baecf8ad649c47fc7faab
XD <Xavier.Dumetz@gmail.com>
2014-03-14 17:28:56 +0100

    MAJ GUI/PanneauControle 14/03/2014-17h25 Ajout des sous panneau.

--- Slope3D_Xav/src/GUI/PanneauControle.java
+++ Slope3D_Xav/src/GUI/PanneauControle.java
@@ -1,13 +1,29 @@
 package GUI;
 
 import java.awt.Color;
+import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
 public class PanneauControle extends JPanel {
-	public PanneauControle() {
+	private PanneauControleDeplacement panTra;
+	private PanneauControleRotation panRot;
+	
+	public PanneauControle(MenuReglage mReg, Dimension d){
 		super();
 		this.setBackground(Color.GRAY);
+		this.setSize(d);
+		this.setLayout(null);
+		
+		panTra = new PanneauControleDeplacement(mReg, new Dimension(d.width, 200));
+		panTra.setLocation(0,0);
+		this.add(panTra);
+		
+		panRot = new PanneauControleRotation(mReg, new Dimension(d.width, d.height-200));
+		panRot.setLocation(0,203);
+		this.add(panRot);
+		
+		this.setVisible(true);
 		
 	}
 }
