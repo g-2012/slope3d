1ea5d0da9655ebb6f48d7dff7fa2d02559b8081e
XD <Xavier.Dumetz@gmail.com>
2014-03-19 14:18:51 +0100

    MAJ 19/03/2014-14:15 Les constantes sont maintenant déclarées en final.

--- Slope3D_Xav/src/Utils/Constantes.java
+++ Slope3D_Xav/src/Utils/Constantes.java
@@ -8,21 +8,21 @@ public class Constantes {
 	/*
 	 * Constantes liées au choix des objets affichés dans l'environnement 3D
 	 */
-	public static byte OBJ_RIEN = 0; // Aucun objet affiché
-	public static byte OBJ_MNT = 1; // Seul le MNT est affiché
-	public static byte OBJ_ISOLIGNES = 2; // Seules les isolignes sont affichées
-	public static byte OBJ_TOUT = 3; // Le MNT et les isolignes sont tous deux affichés
+	public static final byte OBJ_RIEN = 0; // Aucun objet affiché
+	public static final byte OBJ_MNT = 1; // Seul le MNT est affiché
+	public static final byte OBJ_ISOLIGNES = 2; // Seules les isolignes sont affichées
+	public static final byte OBJ_TOUT = 3; // Le MNT et les isolignes sont tous deux affichés
 
 	/*
 	 * Constantes liées au choix du type de caméra
 	 */
-	public static byte CAM_ORBITE = 0; // Caméra en orbite
-	public static byte CAM_DESSUS = 1; // Vue orthogonale
-	public static byte CAM_PERSO = 2; // Navigation dans l'espace
+	public static final byte CAM_ORBITE = 0; // Caméra en orbite
+	public static final byte CAM_DESSUS = 1; // Vue orthogonale
+	public static final byte CAM_PERSO = 2; // Navigation dans l'espace
 	
 	/*
 	 * Constantes liées au choix du code couleurs pour la pente
 	 */
-	public static boolean COU_AUTO = false; // Nuances de gris de valeur proportionnelle à la pente
-	public static boolean COU_PERSO = true; // Plages de couleurs définies par l'utilisateur
+	public static final boolean COU_AUTO = false; // Nuances de gris de valeur proportionnelle à la pente
+	public static final boolean COU_PERSO = true; // Plages de couleurs définies par l'utilisateur
 }
