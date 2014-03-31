c79c6cdfce99f08d4b868842f4f5072f7c2bbb05
XD <Xavier.Dumetz@gmail.com>
2014-03-31 19:20:45 +0200

    Nettoyage du code et des commentaires. Correction d'erreurs d'encodage des caractères.

--- Slope3D_Xav/src/GUI/PanneauControleRotation.java
+++ Slope3D_Xav/src/GUI/PanneauControleRotation.java
@@ -2,15 +2,9 @@ package GUI;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 
 import javax.swing.AbstractButton;
-import javax.swing.BoxLayout;
 import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.SequentialGroup;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -20,20 +14,32 @@ import Utils.Constantes;
 
 public class PanneauControleRotation extends JPanel {
 	/*
-	 * Panneau de contrôle qui permet à l'utilisateur d'effectuer différentes opérations de
-	 * rotation de la caméra suivant le type de vue choisie dans le menu de réglages :
-	 * - En vue orbitale, réglage du sens et de la vitesse de rotation
-	 * - En vue de dessus, aucune rotation autorisée
-	 * - En vue libre, rotation verticale ou horizontale de la caméra, et modification de la sensibilité des commandes
+	 * Panneau de contrÃ´le qui permet Ã  l'utilisateur d'effectuer diffÃ©rentes
+	 * opÃ©rations de rotation de la camÃ©ra suivant le type de vue choisie dans
+	 * le menu de rÃ©glages :
+	 *  - En vue orbitale, rÃ©glage du sens et de la vitesse
+	 * de rotation
+	 *  - En vue de dessus, aucune rotation autorisÃ©e - En vue libre,
+	 * rotation verticale ou horizontale de la camÃ©ra, et modification de la
+	 * sensibilitÃ© des commandes
 	 */
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2116064159072301532L;
 
 	/*
 	 * Attributs : boutons
 	 */
-	JButton bSensHora, bSensAnti, // Boutons de choix du sens : respectivement horaire et antihoraire
-	bVitPlus, bVitMoins,  // Vitesse de rotation / sensibilité des commandes de rotation
-	bHPlus, bHMoins, // Rotation par rapport au plan vertical (dans le plan horizontal)
-	bVPlus, bVMoins; // Rotation par rapport au plan horizontal (dans le plan vertical)
+	JButton bSensHora, bSensAnti, // Boutons de choix du sens : respectivement
+									// horaire et antihoraire
+			bVitPlus, bVitMoins, // Vitesse de rotation / sensibilitÃ© des
+									// commandes de rotation
+			bHPlus, bHMoins, // Rotation par rapport au plan vertical (dans le
+								// plan horizontal)
+			bVPlus, bVMoins; // Rotation par rapport au plan horizontal (dans le
+								// plan vertical)
 	/*
 	 * Autres attributs
 	 */
@@ -42,14 +48,18 @@ public class PanneauControleRotation extends JPanel {
 	/*
 	 * Constructeur
 	 */
-	public PanneauControleRotation(MenuReglage Reglages /* permet de connaitre les valeurs des réglages */, Dimension d) {
+	public PanneauControleRotation(MenuReglage Reglages /*
+														 * permet de connaitre
+														 * les valeurs des
+														 * rÃ©glages
+														 */, Dimension d) {
 		super();
 		this.setPreferredSize(d);
-		this.setBackground(new Color(225,225,225));
+		this.setBackground(new Color(225, 225, 225));
 		this.mReg = Reglages;
 
 		/*
-		 * Création des boutons
+		 * CrÃ©ation des boutons
 		 */
 		bSensHora = new JButton("Horaire");
 		bSensAnti = new JButton("Direct");
@@ -63,21 +73,20 @@ public class PanneauControleRotation extends JPanel {
 		/*
 		 * Dimensionnement des boutons
 		 */
-		bSensHora.setSize(60,60);
-		bSensAnti.setSize(60,60);
-		bVitPlus.setSize(60,60);
-		bVitMoins.setSize(60,60);
-		bVPlus.setSize(60,60);
-		bVMoins.setSize(60,60);
-		bHPlus.setSize(60,60);
-		bHMoins.setSize(60,60);
-
+		bSensHora.setSize(60, 60);
+		bSensAnti.setSize(60, 60);
+		bVitPlus.setSize(60, 60);
+		bVitMoins.setSize(60, 60);
+		bVPlus.setSize(60, 60);
+		bVMoins.setSize(60, 60);
+		bHPlus.setSize(60, 60);
+		bHMoins.setSize(60, 60);
 
 		/*
-		 * Activation ou désactivation des boutons selon la vue choisie
+		 * Activation ou dÃ©sactivation des boutons selon la vue choisie
 		 */
 		if (mReg.getChoixCam() == Constantes.CAM_DESSUS) { // Vue du dessus
-			// Aucune rotation autorisée
+			// Aucune rotation autorisÃ©e
 			bSensAnti.setEnabled(false);
 			bSensHora.setEnabled(false);
 			bVitMoins.setEnabled(false);
@@ -85,10 +94,10 @@ public class PanneauControleRotation extends JPanel {
 			bHPlus.setEnabled(false);
 			bHMoins.setEnabled(false);
 			bVPlus.setEnabled(false);
-			bVMoins.setEnabled(false);			
-		}
-		else if (mReg.getChoixCam() == Constantes.CAM_ORBITE) { // Caméra en orbite
-			// L'utilisateur peut régler le sens et la vitesse de rotation
+			bVMoins.setEnabled(false);
+		} else if (mReg.getChoixCam() == Constantes.CAM_ORBITE) { // CamÃ©ra en
+																	// orbite
+			// L'utilisateur peut rÃ©gler le sens et la vitesse de rotation
 			bSensAnti.setEnabled(true);
 			bSensHora.setEnabled(true);
 			bVitMoins.setEnabled(true);
@@ -96,22 +105,21 @@ public class PanneauControleRotation extends JPanel {
 			bHPlus.setEnabled(false);
 			bHMoins.setEnabled(false);
 			bVPlus.setEnabled(false);
-			bVMoins.setEnabled(false);			
-		}
-		else if (mReg.getChoixCam() == Constantes.CAM_PERSO) { // Vue libre
-			// L'utilisateur fait librement tourner la caméra
-			bSensAnti.setEnabled(false); // Inutile ici
-			bSensHora.setEnabled(false); // Inutile ici
+			bVMoins.setEnabled(false);
+		} else if (mReg.getChoixCam() == Constantes.CAM_PERSO) { // Vue libre
+			// L'utilisateur fait librement tourner la camÃ©ra
+			bSensAnti.setEnabled(false); // Commande inutile ici
+			bSensHora.setEnabled(false); // Commande inutile ici
 			bVitMoins.setEnabled(true);
 			bVitPlus.setEnabled(true);
 			bHPlus.setEnabled(true);
 			bHMoins.setEnabled(true);
 			bVPlus.setEnabled(true);
-			bVMoins.setEnabled(true);			
+			bVMoins.setEnabled(true);
 		}
 
 		/*
-		 * Mise en page et intégration de boutons
+		 * Mise en page et intÃ©gration de boutons
 		 */
 
 		// Gestionnaire de mise en page = GroupLayout
@@ -122,16 +130,16 @@ public class PanneauControleRotation extends JPanel {
 		layout.setAutoCreateContainerGaps(true);
 		layout.setAutoCreateGaps(true);
 
-		// Création des éléments
+		// CrÃ©ation des Ã©lÃ©ments
 		JLabel tRot = new JLabel("Rotation"); // Le titre du panneau
 		tRot.setSize(75, 30);
 		tRot.setBorder(new LineBorder(Color.GRAY, 1));
 		tRot.setHorizontalAlignment(AbstractButton.CENTER);
 		tRot.setVerticalAlignment(AbstractButton.CENTER);
 
-		//Boutons liés à l'angle horizontal
+		// Boutons liÃ©s Ã  l'angle horizontal
 		JLabel tHor = new JLabel("Horizontal"); // Titre
-		tHor.setSize(70,25);
+		tHor.setSize(70, 25);
 		tHor.setHorizontalAlignment(AbstractButton.CENTER);
 		tHor.setVerticalAlignment(AbstractButton.CENTER);
 
@@ -140,9 +148,9 @@ public class PanneauControleRotation extends JPanel {
 		vHor.setHorizontalAlignment(AbstractButton.CENTER);
 		vHor.setVerticalAlignment(AbstractButton.CENTER);
 
-		//Boutons liés à l'angle vertical
+		// Boutons liÃ©s Ã  l'angle vertical
 		JLabel tVer = new JLabel("Vertical"); // Titre
-		tVer.setSize(70,25);
+		tVer.setSize(70, 25);
 		tVer.setHorizontalAlignment(AbstractButton.CENTER);
 		tVer.setVerticalAlignment(AbstractButton.CENTER);
 
@@ -151,9 +159,9 @@ public class PanneauControleRotation extends JPanel {
 		vVer.setHorizontalAlignment(AbstractButton.CENTER);
 		vVer.setVerticalAlignment(AbstractButton.CENTER);
 
-		//Boutons liés au sens de rotation
+		// Boutons liÃ©s au sens de rotation
 		JLabel tSens = new JLabel("Sens"); // Titre
-		tSens.setSize(70,25);
+		tSens.setSize(70, 25);
 		tSens.setHorizontalAlignment(AbstractButton.CENTER);
 		tSens.setVerticalAlignment(AbstractButton.CENTER);
 
@@ -162,9 +170,9 @@ public class PanneauControleRotation extends JPanel {
 		vSens.setHorizontalAlignment(AbstractButton.CENTER);
 		vSens.setVerticalAlignment(AbstractButton.CENTER);
 
-		//Boutons liés à la vitesse
+		// Boutons liÃ©s Ã  la vitesse
 		JLabel tVit = new JLabel("Vitesse"); // Titre
-		tVit.setSize(70,25);
+		tVit.setSize(70, 25);
 		tVit.setHorizontalAlignment(AbstractButton.CENTER);
 		tVit.setVerticalAlignment(AbstractButton.CENTER);
 
@@ -173,100 +181,164 @@ public class PanneauControleRotation extends JPanel {
 		vVit.setHorizontalAlignment(AbstractButton.CENTER);
 		vVit.setVerticalAlignment(AbstractButton.CENTER);
 
-		// Ajout des élements aux groupes :
-		layout.setHorizontalGroup(
-				layout.createSequentialGroup()
+		// Ajout des Ã©lements aux groupes :
+		layout.setHorizontalGroup(layout
+				.createSequentialGroup()
 				.addContainerGap(20, Short.MAX_VALUE)
-				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER,false)
-						.addComponent(tRot)
-						.addGroup(layout.createSequentialGroup()
-								.addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER,false)
-										.addComponent(tHor,70,70,70)
-										.addComponent(bHPlus,60,60,60)
-										.addComponent(vHor,60,60,60)
-										.addComponent(bHMoins,60,60,60)	
-										)
-								.addGap(15)
-								.addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER,false)
-										.addComponent(tVer,70,70,70)
-										.addComponent(bVPlus,60,60,60)
-										.addComponent(vVer,60,60,60)
-										.addComponent(bVMoins,60,60,60)
-										)
-								)
-						.addGroup(layout.createSequentialGroup()
-								.addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER,false)
-										.addComponent(tSens,70,70,70)
-										.addComponent(bSensHora,60,60,60)
-										.addComponent(vSens,60,60,60)
-										.addComponent(bSensAnti,60,60,60)
-										)
-								.addGap(15)
-								.addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER,false)
-										.addComponent(tVit,70,70,70)
-										.addComponent(bVitPlus,60,60,60)
-										.addComponent(vVit,60,60,60)
-										.addComponent(bVitMoins,60,60,60)
-										)						
-								)
-						)
-				.addContainerGap(20, Short.MAX_VALUE)	
-				);
-		layout.setVerticalGroup(
-				layout.createSequentialGroup()
+				.addGroup(
+						layout.createParallelGroup(
+								GroupLayout.Alignment.CENTER, false)
+								.addComponent(tRot)
+								.addGroup(
+										layout.createSequentialGroup()
+												.addGroup(
+														layout.createParallelGroup(
+																GroupLayout.Alignment.CENTER,
+																false)
+																.addComponent(
+																		tHor,
+																		70, 70,
+																		70)
+																.addComponent(
+																		bHPlus,
+																		60, 60,
+																		60)
+																.addComponent(
+																		vHor,
+																		60, 60,
+																		60)
+																.addComponent(
+																		bHMoins,
+																		60, 60,
+																		60))
+												.addGap(15)
+												.addGroup(
+														layout.createParallelGroup(
+																GroupLayout.Alignment.CENTER,
+																false)
+																.addComponent(
+																		tVer,
+																		70, 70,
+																		70)
+																.addComponent(
+																		bVPlus,
+																		60, 60,
+																		60)
+																.addComponent(
+																		vVer,
+																		60, 60,
+																		60)
+																.addComponent(
+																		bVMoins,
+																		60, 60,
+																		60)))
+								.addGroup(
+										layout.createSequentialGroup()
+												.addGroup(
+														layout.createParallelGroup(
+																GroupLayout.Alignment.CENTER,
+																false)
+																.addComponent(
+																		tSens,
+																		70, 70,
+																		70)
+																.addComponent(
+																		bSensHora,
+																		60, 60,
+																		60)
+																.addComponent(
+																		vSens,
+																		60, 60,
+																		60)
+																.addComponent(
+																		bSensAnti,
+																		60, 60,
+																		60))
+												.addGap(15)
+												.addGroup(
+														layout.createParallelGroup(
+																GroupLayout.Alignment.CENTER,
+																false)
+																.addComponent(
+																		tVit,
+																		70, 70,
+																		70)
+																.addComponent(
+																		bVitPlus,
+																		60, 60,
+																		60)
+																.addComponent(
+																		vVit,
+																		60, 60,
+																		60)
+																.addComponent(
+																		bVitMoins,
+																		60, 60,
+																		60))))
+				.addContainerGap(20, Short.MAX_VALUE));
+		layout.setVerticalGroup(layout
+				.createSequentialGroup()
 				.addContainerGap(20, 30)
 				.addComponent(tRot)
 				.addGap(20)
-				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER,false)
-						
-						.addGroup(layout.createSequentialGroup()
-								.addComponent(tHor,25,25,25)
-								.addComponent(bHPlus,60,60,60)
-								.addComponent(vHor,25,25,25)
-								.addComponent(bHMoins,60,60,60)
-								)
-						.addGap(10)		
-						.addGroup(layout.createSequentialGroup()
-								.addComponent(tVer,25,25,25)
-								.addComponent(bVPlus,60,60,60)
-								.addComponent(vVer,25,25,25)
-								.addComponent(bVMoins,60,60,60)
-								)
-						)
-				.addGap(15)
-				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER,false)
-						.addGroup(layout.createSequentialGroup()
-								.addComponent(tSens,25,25,25)
-								.addComponent(bSensHora,60,60,60)
-								.addComponent(vSens,25,25,25)
-								.addComponent(bSensAnti,60,60,60)
-								)
-						.addGap(10)
-						.addGroup(layout.createSequentialGroup()
-								.addComponent(tVit,25,25,25)
-								.addComponent(bVitPlus,60,60,60)
-								.addComponent(vVit,25,25,25)
-								.addComponent(bVitMoins,60,60,60)
-								)
-						)
-				.addContainerGap(20, Short.MAX_VALUE)
-				);
+				.addGroup(
+						layout.createParallelGroup(
+								GroupLayout.Alignment.CENTER, false)
 
+								.addGroup(
+										layout.createSequentialGroup()
+												.addComponent(tHor, 25, 25, 25)
+												.addComponent(bHPlus, 60, 60,
+														60)
+												.addComponent(vHor, 25, 25, 25)
+												.addComponent(bHMoins, 60, 60,
+														60))
+								.addGap(10)
+								.addGroup(
+										layout.createSequentialGroup()
+												.addComponent(tVer, 25, 25, 25)
+												.addComponent(bVPlus, 60, 60,
+														60)
+												.addComponent(vVer, 25, 25, 25)
+												.addComponent(bVMoins, 60, 60,
+														60)))
+				.addGap(15)
+				.addGroup(
+						layout.createParallelGroup(
+								GroupLayout.Alignment.CENTER, false)
+								.addGroup(
+										layout.createSequentialGroup()
+												.addComponent(tSens, 25, 25, 25)
+												.addComponent(bSensHora, 60,
+														60, 60)
+												.addComponent(vSens, 25, 25, 25)
+												.addComponent(bSensAnti, 60,
+														60, 60))
+								.addGap(10)
+								.addGroup(
+										layout.createSequentialGroup()
+												.addComponent(tVit, 25, 25, 25)
+												.addComponent(bVitPlus, 60, 60,
+														60)
+												.addComponent(vVit, 25, 25, 25)
+												.addComponent(bVitMoins, 60,
+														60, 60)))
+				.addContainerGap(20, Short.MAX_VALUE));
 
 		this.setVisible(true);
 
 	}
-	
-	
+
 	/*
-	 * Méthode de rafrachissement des boutons quand les réglages ont été modifiés
+	 * MÃ©thode de rafraichissement des boutons quand les rÃ©glages ont Ã©tÃ©
+	 * modifiÃ©s
 	 */
-	public void rafraichitBoutons(){
+	public void rafraichitBoutons() {
 		/*
-		 * Activation ou désactivation des boutons selon la vue choisie
+		 * Activation ou dÃ©sactivation des boutons selon la vue choisie
 		 */
 		if (mReg.getChoixCam() == Constantes.CAM_DESSUS) { // Vue du dessus
-			// Aucune rotation autorisée
+			// Aucune rotation autorisï¿½e
 			bSensAnti.setEnabled(false);
 			bSensHora.setEnabled(false);
 			bVitMoins.setEnabled(false);
@@ -274,10 +346,10 @@ public class PanneauControleRotation extends JPanel {
 			bHPlus.setEnabled(false);
 			bHMoins.setEnabled(false);
 			bVPlus.setEnabled(false);
-			bVMoins.setEnabled(false);			
-		}
-		else if (mReg.getChoixCam() == Constantes.CAM_ORBITE) { // Caméra en orbite
-			// L'utilisateur peut régler le sens et la vitesse de rotation
+			bVMoins.setEnabled(false);
+		} else if (mReg.getChoixCam() == Constantes.CAM_ORBITE) { // CamÃ©ra en
+																	// orbite
+			// L'utilisateur peut rÃ©gler le sens et la vitesse de rotation
 			bSensAnti.setEnabled(true);
 			bSensHora.setEnabled(true);
 			bVitMoins.setEnabled(true);
@@ -285,10 +357,9 @@ public class PanneauControleRotation extends JPanel {
 			bHPlus.setEnabled(false);
 			bHMoins.setEnabled(false);
 			bVPlus.setEnabled(false);
-			bVMoins.setEnabled(false);			
-		}
-		else if (mReg.getChoixCam() == Constantes.CAM_PERSO) { // Vue libre
-			// L'utilisateur fait librement tourner la caméra
+			bVMoins.setEnabled(false);
+		} else if (mReg.getChoixCam() == Constantes.CAM_PERSO) { // Vue libre
+			// L'utilisateur fait librement tourner la camÃ©ra
 			bSensAnti.setEnabled(false); // Inutile ici
 			bSensHora.setEnabled(false); // Inutile ici
 			bVitMoins.setEnabled(true);
@@ -296,7 +367,7 @@ public class PanneauControleRotation extends JPanel {
 			bHPlus.setEnabled(true);
 			bHMoins.setEnabled(true);
 			bVPlus.setEnabled(true);
-			bVMoins.setEnabled(true);			
+			bVMoins.setEnabled(true);
 		}
 	}
 
