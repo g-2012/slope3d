603ba5cb5d8ce7dd8de65e6eecbcf5a9ce4b7489
XD <Xavier.Dumetz@gmail.com>
2014-03-31 15:58:20 +0200

    Formatage automatique du code.

--- Slope3D_Xav/src/GUI/BarreMenu.java
+++ Slope3D_Xav/src/GUI/BarreMenu.java
@@ -9,15 +9,18 @@ public class BarreMenu extends JMenuBar {
 	 * 
 	 */
 	private static final long serialVersionUID = 6646521212364175899L;
-	/* Cette classe correspond à la barre de menu qui sera affichée dans la fenêtre du programme. */
-	
+	/*
+	 * Cette classe correspond à la barre de menu qui sera affichée dans la
+	 * fenêtre du programme.
+	 */
+
 	/*
 	 * Attributs
 	 */
 	private MenuFichier mFichier;
 	private MenuReglage mReglages;
 	private Fenetre parent;
-	
+
 	/*
 	 * Constructeur.
 	 */
@@ -25,31 +28,32 @@ public class BarreMenu extends JMenuBar {
 		super();
 		this.parent = parent;
 		this.setPreferredSize(d);
-		
-		/* 
-		 * Menu relatif à l'ouverture de fichiers et à la fermeture du programme.
+
+		/*
+		 * Menu relatif à l'ouverture de fichiers et à la fermeture du
+		 * programme.
 		 */
 		mFichier = new MenuFichier(this);
 		this.add(mFichier);
-		
-		/* 
+
+		/*
 		 * Menu donnant accès au paramétrage de la visualisation.
 		 */
 		mReglages = new MenuReglage(this);
 		this.add(mReglages);
 	}
-	
+
 	/*
 	 * Getters
 	 */
 	public MenuReglage getMenuReglage() {
 		return mReglages;
 	}
-	
+
 	public MenuFichier getMenuFichier() {
 		return mFichier;
 	}
-	
+
 	public Fenetre getFenetreMere() {
 		return parent;
 	}
